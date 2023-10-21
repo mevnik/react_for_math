1999115533eed741705a0d82a378eac26e597442
mevnik <mevnik@gmail.com>
2023-10-21 11:11:23 +0200

    delete hover

--- src/Components/main_field3.jsx
+++ src/Components/main_field3.jsx
@@ -46,20 +46,16 @@ function MainField3(props) {
           <div className="mb-3 text-center last" >
 
 
-            <input className="border border-dark-subtle text-center mt-2 cursor" type="text" value={props.number2} id="2" onClick = {onClick} readOnly/>
+            <input className="border border-success text-center mt-2 cursor" type="text" value={props.number2} id="2" onClick = {onClick} readOnly/>
 
           </div>
 
         </Col>
-        <Col xs = {1} md={1}>
-          <div className=" text-center mt-2"></div>
-
-        </Col>
-        <Col xs = {4} md={4}>
+        <Col xs = {5} md={5}>
 
           <div className="border border-success bg-light text-dark mb-3 mt-2 text-center last" >
 
-            <b>Choose what number is more</b>
+            <b>Choose the larger of two numbers</b>
           </div>
 
         </Col>
